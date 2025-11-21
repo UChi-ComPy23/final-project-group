ccc1bc61ea903842210142677bec5ee7551594ac
Ke shen <kshen1@uchicago.edu>
2025-11-20 20:11:09 -0600

    sk_checkpoint

--- test/PG_vs_FISTA.py
+++ test/PG_vs_FISTA.py
@@ -1,3 +1,8 @@
+import sys
+import os
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_root)
+
 import numpy as np
 import matplotlib.pyplot as plt
 import time
